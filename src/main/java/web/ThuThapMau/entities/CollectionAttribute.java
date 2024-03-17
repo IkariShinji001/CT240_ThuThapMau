c76ff7e7f58eac3d04d79394bcda88bc927755d8
IkariShinji001 <duytran2003dev@gmail.com>
2024-03-17 11:27:37 +0700

    Khoi tao entities: Collection, CollectionAttribute, CollectionAttributeId

---
+++ src/main/java/web/ThuThapMau/entities/CollectionAttribute.java
@@ -0,0 +1,18 @@
+package web.ThuThapMau.entities;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import web.ThuThapMau.entities.compositeKeyId.CollectionAttributeId;
+
+@Entity(name = "CollectionAttribute")
+public class CollectionAttribute {
+    @EmbeddedId
+    private CollectionAttributeId id;
+    private String form_attribute_name;
+    @ManyToOne
+    @JoinColumn(name = "form_id", insertable = false, updatable = false)
+    private Form form;
+
+}
