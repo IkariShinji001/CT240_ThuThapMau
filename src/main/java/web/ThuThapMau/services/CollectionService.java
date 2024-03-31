64032079d0e898c3d89511d00a1c04d8deaf2e52
chicken lee <tuankie191103@gmail.com>
2024-03-31 15:05:27 +0700

    chuyen createColletion sang serivce

--- src/main/java/web/ThuThapMau/services/CollectionService.java
+++ src/main/java/web/ThuThapMau/services/CollectionService.java
@@ -1,25 +1,95 @@
 package web.ThuThapMau.services;
 
 
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 import web.ThuThapMau.entities.Collection;
+import web.ThuThapMau.entities.Project;
+import web.ThuThapMau.entities.User;
 import web.ThuThapMau.repositories.CollectionRepository;
+import web.ThuThapMau.services.ProjectService;
 
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
 @Service
 public class CollectionService {
 
+
     @Autowired
     private CollectionRepository collectionRepository;
+    @Autowired
+    private Cloudinary cloudinary;
+    @Autowired
+    UserService userService;
+    @Autowired
+    ProjectService projectService;
     public List<Collection> getAllCollection() {
         return collectionRepository.findAll();
     }
 
-    public Collection createCollection(Collection newCollection) {
-        return collectionRepository.save(newCollection);
+    public Collection createCollection(String collection_name,
+                                       String collection_start,
+                                       String collection_end,
+                                       String collection_description,
+                                       String project_id,
+                                       String user_id,
+                                       MultipartFile file) {
+        BlockingQueue<String> sharedSecureUrlQueue = new ArrayBlockingQueue<>(1);
+
+        Thread uploadThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    System.out.println("Luong Phu");
+                    Map uploadResult = cloudinary.uploader().upload(file.getBytes(), ObjectUtils.emptyMap());
+                    String secureUrl = (String) uploadResult.get("secure_url");
+                    sharedSecureUrlQueue.put(secureUrl);
+                } catch (IOException e){
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        uploadThread.start();
+        try {
+            System.out.println("Luong chinh");
+            Collection newCollection = new Collection();
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+            Date start = dateFormat.parse(collection_start);
+            Date end = dateFormat.parse(collection_end);
+            User user = userService.getUserById(Long.parseLong(user_id)).get();
+
+            Project project  = projectService.getProjectByProjectId(Long.parseLong(project_id));
+
+            newCollection.setUser(user);
+            newCollection.setProject(project);
+            newCollection.setCollection_name(collection_name);
+            newCollection.setCollection_description(collection_description);
+            newCollection.setCollection_start(start);
+            newCollection.setCollection_end(end);
+            System.out.println(newCollection);
+
+            String secureUrl = sharedSecureUrlQueue.take();
+            newCollection.setCollection_image_url(secureUrl);
+            collectionRepository.save(newCollection);
+            return newCollection;
+        } catch ( InterruptedException | ParseException e) {
+            throw new RuntimeException(e);
+        }
+
     }
     public Optional<Collection> getCollectionById(Long collection_id) {
         return collectionRepository.findById(collection_id);
